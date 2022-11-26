25daaff3159bca89d0e2125fe299fd4a78a429f3
VincentGay01 <101732915+VincentGay01@users.noreply.github.com>
2022-11-26 15:43:05 +0100

    page de connexion basique

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CatalogueComponent } from './catalogue/catalogue.component';
+import { ConnexionComponent } from './view/connexion/connexion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CatalogueComponent
+    CatalogueComponent,
+    ConnexionComponent
   ],
   imports: [
     BrowserModule,
